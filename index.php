79e613a3150b9c0d8b07084bd6fc0fe0a1d62502
Linus Laitinen <linus.laitinen@gmail.com>
2015-12-18 12:59:29 +0200

    changes to index

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 	<head>
 		<title>Jeps</title>
 	</head>
-<<<<<<< HEAD
 	<body>test</body>
-=======
 	<body>
 		<h1>Janin tekstiä</h1>
-	
+	<h4>testiätestiä</h4>
 	</body>
->>>>>>> fbcffca02c9f9b409655aadc349aabb303ce742b
+
 </html>
